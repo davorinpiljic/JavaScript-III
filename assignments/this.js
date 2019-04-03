aff84d9101b9bcc0bc50abf61619b6bb31aefc1a
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-04-02 19:51:10 -0600

    this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. implicit, where 'this' references whatever is to the left of the dot when the function is called
+
+* 2. explicit, where what you want 'this' to be is explicitly passed to the function that is processing your parameters
+
+* 3. window, 'this' references whatever is current in the window
+
+* 4. new binding, creates a new constructor function where this references whatever the new object being created is
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +16,49 @@
 
 // code example for Window Binding
 
+console.log(this)
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const someObject = {
+  itemType: 'box',
+  itemColor: 'green',
+  someMethod: function() {
+    return `${this.itemType} is object, color is ${this.itemColor}`
+  }
+}
+
+console.log(someObject.someMethod())
 
 // Principle 3
 
 // code example for New Binding
 
+const House = function(color, style) {
+  this.color = color,
+  this.style = style
+  this.description = function() {
+    console.log(`description of this house is ${this.color} and the style is ${this.style}`)
+   }
+}
+
+const someHouse = new House('blue', 'nice')
+
+someHouse.description()
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const myObj = {
+  color: 'red',
+  name: 'object'
+}
+
+function func(num1) {
+  console.log(`${this.color} is the color and the object name is ${this.name}`)
+}
+
+func.call(myObj)
